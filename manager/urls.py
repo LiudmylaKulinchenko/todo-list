0faffca57f438af7012d27a0320c97eff7b865f6
Liudmyla Kulinchenko <liudmilakulinchenko@gmail.com>
2022-11-14 11:52:17 +0100

    Create tag_list.html

--- manager/urls.py
+++ manager/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from manager.views import TaskList
+from manager.views import TaskList, TagList
 
 urlpatterns = [
     path("", TaskList.as_view(), name="task-list"),
+    path("tags/", TagList.as_view(), name="tag-list"),
 ]
 
 
--- manager/views.py
+++ manager/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.views import generic
 
-from manager.models import Task
+from manager.models import Task, Tag
 
 
 class TaskList(generic.ListView):
     model = Task
     queryset = Task.objects.prefetch_related("tags")
     paginate_by = 3
+
+
+class TagList(generic.ListView):
+    model = Tag
+    paginate_by = 5
